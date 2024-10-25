0b910e7382f94d5e6546b00940c1b21a32ed40e0
Shamil Gurbanli <shamil.gurban@gmail.com>
2024-10-25 19:58:07 +0400

    Fix link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -61,7 +61,7 @@ const Header = () => {
       <div className="card border-0 p-0" style={{ width: '18rem' }}>
         <div className="card-body p-0">
           <div className="list-group m-0">
-            <Link to="/profile" className="list-group-item border-0 list-group-item-action" onClick={closeSubmenu}>
+            <Link to="/DoctorDash" className="list-group-item border-0 list-group-item-action" onClick={closeSubmenu}>
               <i className="far fa-user mr-3"></i>Müraciətlər
             </Link>
             <Link to="/settings" className="list-group-item border-0 list-group-item-action" onClick={closeSubmenu}>
